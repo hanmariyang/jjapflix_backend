dc71eb7b85d74f2e8fdb14d343a5811931650359
devjunseok <wnslgood58@naver.com>
2022-11-07 23:26:53 +0900

    [리팩토링] 코드 리팩토링

--- recommend/urls.py
+++ recommend/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     path('refresh/', views.MovieRefresh.as_view(), name='refresh'),
-    # path('taste/', views.taste, name='taste'),
-    path('taste/', views.TasteView.as_view(), name='taste'),
     path('<int:movie_id>/', views.TasteView.as_view(), name='taste'),
-
 ]
