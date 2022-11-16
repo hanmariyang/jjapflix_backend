864307bf08edca13f56931fe4c906501cfaa936d
devjunseok <wnslgood58@naver.com>
2022-11-16 09:57:00 +0900

    [수정] 회원탈퇴 view 이름 수정

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from users import views
 from dj_rest_auth.registration.views import VerifyEmailView
 
 urlpatterns = [
-    path('delete/', views.UserView.as_view(), name='user_view'),  #회원탈퇴
-    path('<int:user_id>/', views.ProfileView.as_view(), name='profilw_view'), 
+    path('delete/', views.UserDeleteView.as_view(), name='user_view'),  #회원탈퇴
+    path('<int:user_id>/', views.ProfileView.as_view(), name='profilw_view'), # 회원정보 조회
     path('dj-rest-auth/', include('dj_rest_auth.urls')),  # 로그인 및 기타 dj-rest-auth 기능
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')), #회원가입
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
